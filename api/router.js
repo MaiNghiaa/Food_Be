4bf30445ae2bf8931289f65249e18c60976322cc
Trung Nghĩa <109063567+MaiNghiaa@users.noreply.github.com>
2024-06-11 14:28:45 +0700

    Base

---
+++ api/router.js
@@ -0,0 +1,9 @@
+// router.js
+
+"use strict";
+const multer = require("multer");
+const express = require("express");
+const path = require("path");
+const ProductsController = require("./Controller/ProductController");
+
+module.exports = function (app) {};
